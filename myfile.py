76331c00d326437f00f1618be43d187b79ce1ced
Balaji Sankar <balajisankar555@gmail.com>
2024-08-25 19:35:28 +0000

    Added Test Files

--- myfile.py
+++ myfile.py
@@ -1,15 +1,10 @@
 def is_prime(n):
     if n <= 0:
-        print(f"{n} is Invalid Input")
+        return False
     elif n == 1 :
-        print(f"{n} is neither prime or composite")
+        return False
     else:
         for i in range(2, int(n ** 0.5) + 1):
             if n % i == 0:
-                print(f"{n} is not a prime number")
-                return
-        print(f"{n} is not a prime number")
-
-
-
-is_prime(number)
+                return False
+        return True
