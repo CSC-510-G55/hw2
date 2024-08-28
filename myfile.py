dd9fe677b77dde6cf1232bc5b2e988ed2f426c91
Balaji Sankar <balajisankar555@gmail.com>
2024-08-28 06:21:29 +0000

    Test Status & Code Coverage

--- myfile.py
+++ myfile.py
@@ -1,10 +1,9 @@
 def is_prime(n):
-    if n <= 0:
-        return False
-    elif n == 1 :
-        return False
+    res = True
+    if n <= 1 :
+        res = False
     else:
         for i in range(2, int(n ** 0.5) + 1):
             if n % i == 0:
-                return False
-        return True
+                res = False
+    return res
