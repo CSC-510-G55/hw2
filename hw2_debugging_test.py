743e6491fe9b59175d05967e5802f0f7ad7d6df4
Balaji Sankar <balajisankar555@gmail.com>
2024-09-27 20:30:26 +0000

    Seperate Method for Each Test Case

--- hw2_debugging_test.py
+++ hw2_debugging_test.py
@@ -1,14 +1,30 @@
-import hw2_debugging
-import pytest
+"""
+This module tests the merge sort algorithm.
+"""
 
-@pytest.mark.parametrize(
-        ('input','output'),
-        (
-            ([],[]),
-            ([1],[1]),
-            ([5,4,3,2,1,-5,-4,-3,-2,-1],[-5,-4,-3,-2,-1,1,2,3,4,5])
-        )
-)
-def test_is_prime(input, output):
-    assert hw2_debugging.merge_sort(input) == output
-    
+import pytest
+import hw2_debugging
+
+
+@pytest.mark.parametrize("input, output", [
+    ([], []),
+])
+def test_merge_sort_empty_list(inp, out):
+    """Test the function with Empty Array"""
+    assert hw2_debugging.merge_sort(inp) == out
+
+
+@pytest.mark.parametrize("input, output", [
+    ([1], [1]),
+])
+def test_merge_sort_single_element(inp, out):
+    """Test the function with Single Element Array"""
+    assert hw2_debugging.merge_sort(inp) == out
+
+
+@pytest.mark.parametrize("input, output", [
+    ([5, 4, 3, 2, 1, -5, -4, -3, -2, -1], [-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]),
+])
+def test_merge_sort_mixed_elements(inp, out):
+    """Test the function with Mixed Input Array"""
+    assert hw2_debugging.merge_sort(inp) == out
